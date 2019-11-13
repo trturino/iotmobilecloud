75a568c1d7af120d9111125483f2c0d7d481ddc9
Rafael Turino <rturino@cylance.com>
2019-11-12 18:44:44 -0600

    Add mobile project

---
+++ src/mobile/trtrurino.iot.mobile/trtrurino.iot.mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using trtrurino.iot.mobile.Models;
+
+namespace trtrurino.iot.mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
